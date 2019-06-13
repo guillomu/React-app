4cc583e202aa412ab77d47e204ff51f9138617c2
GuiGui <groult_guigui@hotmail.com>
2019-06-13 15:26:22 +0200

    Updates

---
+++ precache-manifest.b7140c28d1d7b4b0e63370dd91a5cda7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7796adc6bce1008972551344d2fc7d2",
+    "url": "/React-app/index.html"
+  },
+  {
+    "revision": "cdc6b75a3a813912973c",
+    "url": "/React-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "822d0c119755a1d7b2ea",
+    "url": "/React-app/static/css/main.8dac1338.chunk.css"
+  },
+  {
+    "revision": "cdc6b75a3a813912973c",
+    "url": "/React-app/static/js/2.51e4d60e.chunk.js"
+  },
+  {
+    "revision": "822d0c119755a1d7b2ea",
+    "url": "/React-app/static/js/main.6557360e.chunk.js"
+  },
+  {
+    "revision": "d704105685e9a50e8fe8",
+    "url": "/React-app/static/js/runtime~main.a7bc867e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
